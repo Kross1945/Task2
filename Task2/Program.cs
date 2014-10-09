8a3f2a2184d0239bae36b18dcd68691d95fc7dce
Kross1945 <kross1945@mail.ru>
2014-10-09 22:20:44 +0400

    Задание!!

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,7 +10,27 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            dsdsds
+            int weightByStr = 20;
+
+            Console.Write("Введите силу персонажа...");
+            string strStr = Console.ReadLine();
+            int str = int.Parse(strStr);
+
+            Console.Write("Введите вес вещей...");
+            string strWeight = Console.ReadLine();
+            int weight = int.Parse(strStr);
+
+            if (weight > str * weightByStr)
+            {
+                Console.WriteLine("Перегружен");
+            }
+            else
+            {
+                Console.WriteLine("Норма");
+            }
+            Console.ReadKey();
+
+          
         }
     }
 }
